511d43415c1f751993db02b61dbb4ee24f05ed21
Anastasiia Ünal <velyka.nastya@icloud.com>
2022-12-23 02:08:22 +0100

    skills set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  // { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
