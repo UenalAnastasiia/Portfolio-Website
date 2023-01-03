55af1c4f27b2863c23993ab9caaf485b7bbfe12a
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-01-03 15:14:08 +0100

    clean code skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,10 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  skills = [
+    {
+      'img': 'javascript.png',
+      'name': 'JavaScript'
+    },
+
+    {
+      'img': 'angular.png',
+      'name': 'Angular'
+    },
+    
+    {
+      'img': 'typescript.png',
+      'name': 'TypeScript'
+    },
+    
+    {
+      'img': 'html.png',
+      'name': 'HTML'
+    },
+    
+    {
+      'img': 'css.png',
+      'name': 'CSS'
+    },
+    
+    {
+      'img': 'scrum.png',
+      'name': 'SCRUM'
+    },
+    
+    {
+      'img': 'git.png',
+      'name': 'Git'
+    },
+    
+    {
+      'img': 'design.png',
+      'name': 'Material Design'
+    },
+    
+    {
+      'img': 'api.png',
+      'name': 'Rest API'
+    },
+
+    {
+      'img': 'firebase.png',
+      'name': 'Firebase'
+    }
+  ]
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
