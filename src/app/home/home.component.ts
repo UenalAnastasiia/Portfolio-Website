26efb4c0fd777f332fcd4e838a28ee09949cd855
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-01-10 21:03:28 +0100

    responsive home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { MouseOverService } from '../services/mouse-over.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', './circle.scss']
 })
 export class HomeComponent implements OnInit {
 
